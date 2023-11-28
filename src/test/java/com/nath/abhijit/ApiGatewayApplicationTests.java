63b0271746d80b7f75921d0728ecb89ff2d2ac50
unknown <nath.abhijit1990@gmail.com>
2023-11-28 18:00:01 +0530

    initial commit of api-gateway

---
+++ src/test/java/com/nath/abhijit/ApiGatewayApplicationTests.java
@@ -0,0 +1,12 @@
+package com.nath.abhijit;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	/*@Test
+	void contextLoads() {
+	}*/
+
+}
